1b98db2a6d072a55ece9e02192af77c67ee637bf
lierxing <15510223171@163.com>
2022-01-19 19:46:04 +0800

    vue路由中replace和push的区别

---
+++ src/views/vue_router_replace/a.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="a">
+    <h5>a 页面</h5>
+    <button @click="handleJump">jump</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "A",
+  data() {
+    return {};
+  },
+  methods: {
+    handleJump() {
+      console.log("jump");
+      this.$router.push({
+        name: "B",
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.a {
+  background-color: #fff;
+}
+</style>
---
+++ src/views/vue_router_replace/d.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d">
+    <h5>d页面</h5>
+    <button @click="handleBack">返回</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "D",
+  data() {
+    return {};
+  },
+  methods: {
+    handleBack() {
+      console.log("handleBack");
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.d {
+  background-color: #fff;
+}
+</style>
