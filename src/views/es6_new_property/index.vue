6ae32f06f2359e7a8395a6007b05606d6f4ba022
w_lierxing <15510223171@163.com>
2022-03-02 15:48:57 +0800

    es6新特性

---
+++ src/views/es6_new_property/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="es6-new-property">
+    <!-- es6-new-property -->
+    <p>
+      <strong>说出ES6的新特性，并简述它们的作用。</strong> <br />
+      （1） let关键字，用于声明只在块级作用域起作用的变量。 <br />
+      （2） const关键字，用于声明一个常量。<br />
+      （3）
+      结构赋值，一种新的变量赋值方式。常用于交换变量值，提取函数返回值，设置默认值。<br />
+      （4）Symbol数据类型，定义一个独一无二的值。<br />
+      （5）Proxy代理，用于编写处理函数，来拦截目标对象的操作。<br />
+      （6）for...of遍历，可遍历具有iterator 接口的数据结构。<br />
+      （7）Set结构，存储不重复的成员值的集合。<br />
+      （8）Map结构，键名可以是任何类型的键值对集合。<br />
+      （9）Promise对象，更合理、规范地处理异步操作。<br />
+      （10）Class类定义类和更简便地实现类的继承。<br />
+    </p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.es6-new-property {
+  background-color: #fff;
+}
+</style>
